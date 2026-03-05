b07ea193342ed21ab6102ca256c265d5fc013c43
Ujjwalban <ujju.ban@gmail.com>
2026-03-06 01:52:18 +0530

    stripe refactored

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,5 +1,11 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2023-10-16', // or latest
+if (!process.env.STRIPE_SECRET_KEY) {
+    if (process.env.NODE_ENV === 'production') {
+        throw new Error('STRIPE_SECRET_KEY is missing in production!')
+    }
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || 'sk_test_mock', {
+    apiVersion: '2023-10-16' as any, // Cast to any to handle potential version mismatches in very new stripe package
 })
